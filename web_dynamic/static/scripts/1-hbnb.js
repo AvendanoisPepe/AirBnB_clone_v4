161748fbd28d4a44a823d8bb5d4860f0b0994073
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-06-03 23:32:28 -0500

    todo de nuevo pero con js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,24 +1,17 @@
-$(() => {
-  //verificamos que el doom este cargado
-  const listados = {};
-  let eliminar = [];
-  //generamos el evento
-  $('.amenities .popover input').change(function () {
-    //accedemos a la ubicacion del elemento y obtenemos su valor
-    if ($(this).prop('checked')) {
-      //agregamos a la lista el valor obtenido
-      listados[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else if (!$(this).prop('checked')) {
-      //elimino las propiades del objeto listado o el contenido
-      delete listados[$(this).attr('data-id')];
-    }
-    //guardamos la matriz de listados ya ordenada
-    eliminar = Object.values(listados).sort();
-    //si hay al menos uno selecionado la imprimi y agrega la ,
-    if (eliminar.length != 0)
-      $('.amenities h4').text(eliminar.join(', '));
-    else
-      //si no deja tal cual
-      $('.amenities h4').html("&nbsp;");
-  });
-});
+const servicios1 = function () {
+    let pepe = document.getElementById("hugo");
+    let mie = document.querySelectorAll(".miedo");
+    let ap = document.getElementById("mk")
+
+    pepe.addEventListener("click", function () {
+        let selecionad = "";
+        mie.forEach((e) => {
+            if (e.checked == true) {
+                selecionad += e.getAttribute("data-name") + ", ";
+            }
+        })
+            ap.innerText = selecionad;
+    })
+}
+
+servicios1();
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,47 +14,21 @@ const burbujita = function () {
     )
 };
 
-const servicios = function () {
-    //verificamos que el doom este cargado
-    const listados = {};
-    let eliminar = [];
-    //generamos el evento
-    $('.amenities .popover input').change(function () {
-    //accedemos a la ubicacion del elemento y obtenemos su valor
-    if ($(this).prop('checked')) {
-        //agregamos a la lista el valor obtenido
-        listados[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else if (!$(this).prop('checked')) {
-        //elimino las propiades del objeto listado o el contenido
-        delete listados[$(this).attr('data-id')];
-    }
-    //guardamos la matriz de listados ya ordenada
-    eliminar = Object.values(listados).sort();
-    //si hay al menos uno selecionado la imprimi y agrega la ,
-    if (eliminar.length != 0)
-        $('.amenities h4').text(eliminar.join(', '));
-    else
-        //si no deja tal cual
-        $('.amenities h4').html("&nbsp;");
-  });
-}
 const servicios1 = function () {
-    const listados = {};
-    let eliminar = [];
-
     let pepe = document.getElementById("hugo");
     let mie = document.querySelectorAll(".miedo");
     let ap = document.getElementById("mk")
-    
-        pepe.addEventListener("change", function () {
+
+    pepe.addEventListener("click", function () {
+        let selecionad = "";
         mie.forEach((e) => {
-        if (e.checked == true) {
-            console.log(mie[9].attributes[2]);
-            //let hee = document.createElement('p');
-            //hee.innerHTML = e
-        }
+            if (e.checked == true) {
+                selecionad += e.getAttribute("data-name") + ", ";
+            }
         })
+            ap.innerText = selecionad;
     })
 }
+
 servicios1();
 burbujita();
