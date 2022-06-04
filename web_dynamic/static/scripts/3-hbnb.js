07aaf6389d1163be400c82fb0827a283cd846bdf
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-06-04 18:02:23 -0500

    carlos haga este

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,39 @@
+const burbujita = function () {
+    var cabecera = document.getElementById('api_status');
+    fetch("http://172.20.244.135:5001/api/v1/status/").then(
+        data => { return data.json() }
+    ).then(
+        pedro => {
+            console.log(pedro.status);
+            if (pedro.status === "OK") {
+                cabecera.classList.add('available');
+            } else {
+                cabecera.classList.remove('available');
+            }
+        }
+    )
+};
+
+const servicios1 = function () {
+    let pepe = document.getElementById("hugo");
+    let mie = document.querySelectorAll(".miedo");
+    let ap = document.getElementById("mk")
+
+    pepe.addEventListener("click", function () {
+        let selecionad = "";
+        mie.forEach((e) => {
+            if (e.checked == true) {
+                selecionad += e.getAttribute("data-name") + ", ";
+            }
+        })
+            ap.innerText = selecionad;
+    })
+}
+
+const posito = function () {
+    
+}
+
+posito();
+servicios1();
+burbujita();
