2ef80870eaaa326b4f86beff5c041dc82446f231
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-06-05 17:04:51 -0500

    fix 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,6 +1,6 @@
 const burbujita = function () {
     var cabecera = document.getElementById('api_status');
-    fetch("http://172.20.252.43:5001/api/v1/status/").then(
+    fetch("http://localhost:5001/api/v1/status/").then(
         data => { return data.json() }
     ).then(
         pedro => {
@@ -45,14 +45,14 @@ function fetchPlaces() {
     return response.json(); // parses JSON response into native JavaScript objects
   }
 
-  postData('http://172.20.252.43:5001/api/v1/places_search/')
+  postData('http://localhost:5001/api/v1/places_search/')
     .then(data => {
       console.log(data[0]['name']); // JSON data parsed by `data.json()` call
       for (const r of data) {
         console.log(r.name);
         var lugar=document.getElementById("hugomrk");
-        let element = document.createElement("pre");
-        let textohtml = "<article><div class='title_box'><h2>" + r.name + "</h2><div class='price_by_night'>$" + r.price_by_night + "</div></div><div class='information'><div class='max_guest'> " + r.max_guest + " Guests</div><div class='number_rooms' > " + r.number_rooms + " Bedrooms</div><div class='number_bathrooms' > " + r.number_bathrooms + " Bathrooms</div></div><div class='user'><b>Owner:</b>" + r.user_id + "</div><div class='description'>" + r.description + "</div></article>";
+        let element = document.createElement("article");
+        let textohtml = "<div class='title_box'><h2>" + r.name + "</h2><div class='price_by_night'>$" + r.price_by_night + "</div></div><div class='information'><div class='max_guest'> " + r.max_guest + " Guests</div><div class='number_rooms' > " + r.number_rooms + " Bedrooms</div><div class='number_bathrooms' > " + r.number_bathrooms + " Bathrooms</div></div><div class='user'><b>Owner:</b>" + r.user_id + "</div><div class='description'>" + r.description + "</div>";
         element.innerHTML=textohtml;
         lugar.appendChild((element))
     } return data;
