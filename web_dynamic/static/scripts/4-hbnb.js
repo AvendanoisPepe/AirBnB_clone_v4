deff30168f1d5833ef5fd4e4dd66a12382a7c185
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-06-09 00:19:36 -0500

    el ultimo

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,83 +1,77 @@
-$(function () {
-    // Serve API content for api_status
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-      if (data['status'] === 'OK') {
-        $('DIV#api_status').addClass('available');
-      } else {
-        $('DIV#api_status').removeClass('available');
-      }
+const but = document.querySelector("button");
+let data = {};
+
+const burbujita = function () {
+    var cabecera = document.getElementById('api_status');
+    fetch("http://localhost:5001/api/v1/status/").then(
+        data => { return data.json() }
+    ).then(
+        pedro => {
+            if (pedro.status === "OK") {
+                cabecera.classList.add('available');
+            } else {
+                cabecera.classList.remove('available');
+            }
+        }
+    )
+};
+
+let carlos = [];
+
+const servicios1 = function () {
+    let pepe = document.getElementById("hugo");
+    let mie = document.querySelectorAll(".miedo");
+    let ap = document.getElementById("mk")
+
+    pepe.addEventListener("click", function () {
+      let selecionad = "";
+      carlos = [];
+        mie.forEach((e) => {
+            if (e.checked == true) {
+              selecionad += e.getAttribute("data-name") + ", ";
+              carlos.push(e.getAttribute("data-id"));
+            }
+        })
+            ap.innerText = selecionad;
+    })
+}
+function fetchPlaces() {
+  // Ejemplo implementando el metodo POST:
+  async function postData(url = '') {
+    // Opciones por defecto estan marcadas con un *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
-    // checkbox and display amenities
-    let dict = {};
-    $('input').change(function () {
-      if (this.checked) {
-        dict[($(this).attr('data-id'))] = $(this).attr('data-name');
-      } else {
-        delete dict[$(this).attr('data-id')];
-      }
-      let arr = '';
-      let separator = '';
-      for (let i in dict) {
-        arr += separator;
-        arr += dict[i];
-        separator = ', ';
-      }
-      if (arr === '') {
-        $('div.amenities h4').text(String.fromCharCode(160));
-      } else {
-        $('div.amenities h4').text(arr);
-      }
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  postData('http://localhost:5001/api/v1/places_search/')
+    .then(data => {
+
+      for (const r of data) {
+        let lugar=document.getElementById("hugomrk");
+        let element = document.createElement("article");
+        let textohtml = "<div class='title_box'><h2>" + r.name + "</h2><div class='price_by_night'>$" + r.price_by_night + "</div></div><div class='information'><div class='max_guest'> " + r.max_guest + " Guests</div><div class='number_rooms' > " + r.number_rooms + " Bedrooms</div><div class='number_bathrooms' > " + r.number_bathrooms + " Bathrooms</div></div><div class='user'><b>Owner:</b>" + r.user_id + "</div><div class='description'>" + r.description + "</div>";
+        element.innerHTML=textohtml;
+        lugar.appendChild((element))
+    } return data;
+    }).then (dato => {
     });
-  
-    // Search when button is clicked
-    $('button').click(function () {
-      let amenityIdList = [];
-      for (let i in dict) {
-        amenityIdList.push(i);
-      }
-      let postDict = {};
-      postDict['amenities'] = amenityIdList;
-      $.ajax({
-        type: 'POST',
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        data: JSON.stringify(postDict),
-        success: function (result) {
-          $('article').remove();
-          for (let i in result) {
-            let structure = [
-              '<article>',
-              '<div class="title">',
-              '<h2>' + result[i].name + '</h2>',
-              '<div class="price_by_night">' + '$' + result[i].price_by_night + '</div>',
-              '</div>',
-              '<div class="information">',
-              '<div class="max_guest">',
-              '<i class="fa fa-users fa-3x" aria-hidden="true"></i>',
-              '<br />',
-              result[i].max_guest + ' Guests',
-              '</div>',
-              '<div class="number_rooms">',
-              '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>',
-              '<br />',
-              result[i].number_rooms + ' Bedrooms',
-              '</div>',
-              '<div class="number_bathrooms">',
-              '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>',
-              '<br />',
-              result[i].number_bathrooms + ' Bathroom',
-              '</div>',
-              '</div>',
-              '<div class="description">',
-              result[i].description,
-              '</div>',
-              '</article>'
-            ];
-            $(structure.join('')).appendTo('section.places');
-          }
-        },
-        dataType: 'json',
-        contentType: 'application/json'
-      });
-    });
-  });
-  
+}
+
+but.addEventListener("click", async function() {
+  data = { amenities: carlos };
+  let lugar = document.getElementById("hugomrk");
+  lugar.textContent = "";
+  console.log(data);
+  fetchPlaces();  
+})
+
+fetchPlaces();
+servicios1();
+burbujita();
